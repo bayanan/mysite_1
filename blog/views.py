c894109f36a3dab06f5e5b4a3b4ae7bb498cbc9b
Yana <ba.ya.na@mail.ru>
2022-08-11 13:21:20 +0500

    using the View class

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-def post_list(request):
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+"""def post_list(request):
     object_list = Post.published.all()
     # По 3 статьи на странице
     paginator = Paginator(object_list, 3)
@@ -16,7 +23,7 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)  # Если номер страницы больше, чем их количество,
         # возвращаем последнюю
     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
-
+"""
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
