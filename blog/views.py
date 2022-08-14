b40a27a53d0d463da15e95938219c9006de1cb2b
Yana <ba.ya.na@mail.ru>
2022-08-14 12:51:07 +0500

    added similarity search

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 """class PostListView(ListView):
     queryset = Post.published.all()
@@ -100,9 +99,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
             results = Post.objects.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gte=0.3).order_by('-similarity')
     return render(request, 'blog/post/search.html', {'form': form,
                                                      'query': query,
                                                      'results': results})
